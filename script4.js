f0d152561a1cee36a89f8300d411d5011e592ae0
Ben Muchiri <benmnjoroge98@gmail.com>
2020-04-13 11:40:20 -0700

    tictactoe

---
+++ script4.js
@@ -0,0 +1,115 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMPINATIONS = [
+    [0,1,2,3],
+    [4,5,6,7],
+    [8,9,10,11],
+    [12,13,14,15],
+    [0,4,8,12],
+    [1,5,9,13],
+    [2,6,10,14],
+    [3,7,11,15],
+    [0,5,10,15],
+    [3,6,9,12],
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+const winningPointsX = document.querySelector('[xwins]')
+const winningPointsO = document.querySelector('[owins]')
+let circleTurn
+let xwins = []
+let owins = []
+
+startGame()
+
+restartButton.addEventListener('click' , startGame)
+
+function playAudio(url) {
+  new Audio(url).play()
+}
+
+function startGame() {
+    circleTurn = false
+    cellElements.forEach(cell => {
+   	cell.classList.remove(X_CLASS)
+   	cell.classList.remove(CIRCLE_CLASS)
+   	cell.removeEventListener('click' , handleClick)
+	cell.addEventListener('click', handleClick, { once: true})
+})
+   setBoardHoverClass()
+   winningMessageElement.classList.remove('show')
+}
+
+function handleClick(e) {
+	//placemark
+   //check for win
+   //check draw
+   //switch turns
+	const cell = e.target
+	const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+	placeMark(cell, currentClass)
+	if (checkWin(currentClass)) {
+       endGame(false)
+	} else if (isDraw()) {
+		endGame(true)
+	} else {
+	swapTurns()
+    setBoardHoverClass()
+	}
+    
+}
+
+function endGame(draw) {
+	if (draw) {
+        winningMessageTextElement.innerText = 'Draw!'
+	} else {
+		winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+    if (circleTurn) {
+       owins.push(1)
+       olength = owins.length
+       winningPointsO.innerText = olength
+    } else {
+       xwins.push(1)
+       xlength = xwins.length
+       winningPointsX.innerText = xlength
+    }
+	}
+	playAudio('img/mysound3.mp3')
+	winningMessageElement.classList.add('show')
+}
+
+function isDraw(){
+	return [...cellElements].every(cell => {
+		return cell.classList.contains(X_CLASS) ||
+		cell.classList.contains(CIRCLE_CLASS)
+	})
+}
+
+function placeMark(cell , currentClass) {
+	cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+	circleTurn = !circleTurn
+}
+
+function setBoardHoverClass(){
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if (circleTurn) {
+       board.classList.add(CIRCLE_CLASS)
+    } else {
+    	board.classList.add(X_CLASS)
+    }
+}
+
+function checkWin(currentClass){
+  return WINNING_COMPINATIONS.some(combination => {
+  	 return combination.every(index => {
+  	 	return cellElements[index].classList.contains(currentClass)
+  	 })
+  })
+}
